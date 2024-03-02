de0e1e91a2c16c2c5e38867dfe060d4009ec8e3c
sagar8912 <sagarsaini9844@gmail.com>
2024-03-02 11:32:14 +0530

    Added utils file

---
+++ utils.py
@@ -0,0 +1,4 @@
+# You need to define preprocessing functions here based on your data preprocessing steps in heart_data_prediction.ipynb
+def preprocess_data(input_data):
+    # Implement your preprocessing steps here
+    return input_data
